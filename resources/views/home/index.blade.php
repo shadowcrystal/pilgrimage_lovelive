e6d7f71ce6548aa756e6a8a3ae3125fba1b83350
titlenono <the.s.k.b0928str1209@gmail.com>
2019-10-31 15:56:08 +0900

    000 経路表示

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,11 @@
 @extends('layout')
+
+@section('content')
+  <section class="indexWrap layoutContainer">
+    <div class="indexContainar container">
+      <div class="siteWrap">
+
+      </div>
+    </div>
+  </section>
+@endsection
